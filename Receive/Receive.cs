f263024989d9c8b55d079e1c2e644a98f8f92ff8
Carlos Silva <c.moreiradeassis@gmail.com>
2021-07-03 13:14:28 -0300

    Versão inicial

---
+++ Receive/Receive.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main()
+        {
+            Contato contato = new Contato();
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "cadastro",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+
+                        contato = JsonConvert.DeserializeObject<Contato>(message);
+
+                        Console.WriteLine($"Nome: {contato.Nome}, CPF: {contato.CPF}, Endereço: {contato.Endereco}, Telefone: {contato.Telefone}");
+                    };
+
+                    channel.BasicConsume(queue: "cadastro",
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+
+    public class Contato
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main()
+        {
+            Contato contato = new Contato();
+
+            string mensagemRabbit = JsonConvert.SerializeObject(contato);
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "cadastro",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                   // string message = "Primeira requisição RabbitMQ";
+
+                    var body = Encoding.UTF8.GetBytes(mensagemRabbit);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "cadastro",
+                                         basicProperties: null,
+                                         body: body);
+                    
+                    Console.WriteLine(" [x] Message sent.");
+                }
+            }
+
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+
+    public class Contato
+    {
+        public Contato()
+        {
+            Nome = "Flavio Cunha";
+            CPF = "123.456.789-00";
+            Endereco = "Rua dos Testes";
+            Telefone = "(11) 9 9107-1256";
+        }
+
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Endereco { get; set; }
+        public string Telefone { get; set; }
+    }
+}
